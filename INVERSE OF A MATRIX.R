be04774ee08080e3a157d99234acb4cb9c456376
Neha <53961604+aarneha@users.noreply.github.com>
2019-08-24 14:11:54 +0530

    Inverse
    Inverse

---
+++ INVERSE OF A MATRIX.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()) {
+  +     inv <- NULL
+  +     set <- function(y) {
+    +         x <<- y
+    +         inv <<- NULL
+    +     }
+  +     get <- function() x
+  +     setInverse <- function(inverse) inv <<- inverse
+  +     getInverse <- function() inv
+  +     list(set = set,
+             +          get = get,
+             +          setInverse = setInverse,
+             +          getInverse = getInverse)
+  + }
+> 
+  cacheSolve <- function(x, ...) {
+    m <- x$getInverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- Inverse(data, ...)
+    x$setInverse(m)
+    m
+  }
